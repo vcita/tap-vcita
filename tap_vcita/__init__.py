7bf0a84d93742d49da26bec0ea5ca7fd1700f9fb
Anton Kagan <anton@vcita.com>
2021-03-14 14:20:03 -0500

    Initial commit

---
+++ tap_vcita/__init__.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+import os
+import json
+import singer
+import requests
+from singer import utils, metadata
+from singer.catalog import Catalog, CatalogEntry
+from singer.schema import Schema
+
+
+PAGE_SIZE = 1
+REQUIRED_CONFIG_KEYS = ["start_date", "username", "password"]
+LOGGER = singer.get_logger()
+
+def get_abs_path(path):
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
+
+def load_schemas():
+    """ Load schemas from schemas folder """
+    schemas = {}
+    for filename in os.listdir(get_abs_path('schemas')):
+        path = get_abs_path('schemas') + '/' + filename
+        file_raw = filename.replace('.json', '')
+        with open(path) as file:
+            schemas[file_raw] = Schema.from_dict(json.load(file))
+    return schemas
+
+
+def discover():
+    raw_schemas = load_schemas()
+    streams = []
+    for stream_id, schema in raw_schemas.items():
+        # TODO: populate any metadata and stream's key properties here..
+        stream_metadata = []
+        key_properties = ["id"]
+        replication_key = "updated_at"
+        streams.append(
+            CatalogEntry(
+                tap_stream_id=stream_id,
+                stream=stream_id,
+                schema=schema,
+                key_properties=key_properties,
+                metadata=stream_metadata,
+                replication_key=replication_key,
+                is_view=None,
+                database=None,
+                table=None,
+                row_count=None,
+                stream_alias=None,
+                replication_method=None,
+            )
+        )
+    return Catalog(streams)
+
+
+def send_request(stream_name, state):
+    url = 'http://localhost:7100/platform/v1/taps'
+    headers = {'Authorization' : 'Admin kjh7tdewtfvdewolmmd' }
+    params = {'model' : stream_name, 'state' : state[stream_name], 'page_size' : PAGE_SIZE}
+    r = requests.get(url, headers=headers, params=params) 
+    data = r.json() 
+    return data['data']
+
+def sync(config, state, catalog):
+    """ Sync data from tap source """
+    # Loop over selected streams in catalog
+    for stream in catalog.get_selected_streams(state):
+        LOGGER.info("Syncing stream:" + stream.tap_stream_id)
+        LOGGER.info("Starting with state %s", state)
+
+        bookmark_column = stream.replication_key
+        is_sorted = True  # TODO: indicate whether data is sorted ascending on bookmark value
+
+        singer.write_schema(
+            stream_name=stream.tap_stream_id,
+            schema=stream.schema.to_dict(),
+            key_properties=stream.key_properties,
+        )
+
+        tap_data = send_request(stream.tap_stream_id, state)
+
+        max_bookmark = None
+        for row in tap_data:
+            # TODO: place type conversions or transformations here
+
+            # write one or more rows to the stream:
+            singer.write_records(stream.tap_stream_id, [row])
+            if bookmark_column:
+                if is_sorted:
+                    # update bookmark to latest value
+                    singer.write_state({stream.tap_stream_id: row[bookmark_column]})
+                else:
+                    # if data unsorted, save max value until end of writes
+                    max_bookmark = max(max_bookmark, row[bookmark_column])
+        if bookmark_column and not is_sorted:
+            singer.write_state({stream.tap_stream_id: max_bookmark})
+    return
+
+
+@utils.handle_top_exception(LOGGER)
+def main():
+    # Parse command line arguments
+    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
+
+    # If discover flag was passed, run discovery mode and dump output to stdout
+    if args.discover:
+        catalog = discover()
+        catalog.dump()
+    # Otherwise run in sync mode
+    else:
+        if args.catalog:
+            catalog = args.catalog
+        else:
+            catalog = discover()
+        sync(args.config, args.state, catalog)
+
+
+if __name__ == "__main__":
+    main()
